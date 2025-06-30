fa47ad685351a427adff68567108596aff5f5fd9
Bhagya Vardhan <bhagyavardhan13@gmail.com>
2025-06-30 12:48:29 +0000

    new thing

--- react learn/src/App.jsx
+++ react learn/src/App.jsx
@@ -1,8 +1,10 @@
-//single element should be returned 
+//single element should be returned, hence <></> used 
+import Chai from './chai'
 function App() {
   return (      
       <>   
       <h1>chai aur react</h1>
+      <Chai />
       <p>yo guys</p>
       </>
   )
---
+++ react learn/src/chai.jsx
@@ -0,0 +1,9 @@
+function Chai() {
+  return (      
+      <>   
+      <h2>sab badiya hai</h2>
+      </>
+  )
+}
+
+export default Chai
