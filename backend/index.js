8b59f4e4b58240df56aa54375573efe4030b6cc2
Bhagya Vardhan <bhagyavardhan13@gmail.com>
2025-07-15 11:44:08 +0000

    ooo

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 // Create HTTP server
 const server = http.createServer(app);
